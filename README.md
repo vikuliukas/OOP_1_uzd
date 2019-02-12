710cc5990e7ca60e9aa6259d00ccb708d89f1c58
vikuliukas <viktorija.kabelyte@gmail.com>
2019-02-12 21:04:26 +0200

    galutine versija

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# OOP_1_uzd
+# Iremintas pasisveikinimas (1 oji užduotis)
+
+![version][version-badge]
+
+## Programos tikslas
+
+-Atspausdinti ireminta varda.
+-Leisti pasirinkti remelio dydi.
+-Skirtingai kreiptis i moteri ar vyra.
+
+## Idiegimas (Unix kompiuteryje)
+
+- `git clone https://github.com/vikuliukas/OOP_1_uzd.git`
+- `cd OOP_1_uzd`
+- `make`
+- `./pasisveikinimas`
+
+## Versiju istorija
+
+### [v1.0](https://github.com/vikuliukas/OOP_1_uzd/releases/tag/v1.0) - (2019-02-12)
+
+**Prideta**
+- Atsirado galimybe kontroliuoti remelio dydi
+
+**Koreguota**
+- Atnaujintas **README** failas
+
+### [v0.2](https://github.com/vikuliukas/OOP_1_uzd/releases/tag/v0.2) - (2019-02-12)
+
+**Koreguota**
+- Programa i moteri ar vyra kreipiasi skirtingai
+
+### [v0.1](https://github.com/vikuliukas/OOP_1_uzd/releases/tag/v0.1) - (2019-02-12)
+
+***Prideta***
+- Pirmine programos versija
--- pasisveikinimas.cpp
+++ pasisveikinimas.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
 #include <string>
 
-int main() {
-	int ilgis;
-	std::string vardas, pirmaeil, antraeil, treciaeil, ketvirtaeil, penktaeil;
-	std::cout << "iveskite varda: ";
-	std::cin >> vardas;
-	if (vardas[vardas.size()-1] == 's') {
-		ilgis = 14 + vardas.size();
-		treciaeil = "* Sveikas, " + vardas + "! *";
-	}
-	else {
-		ilgis = 13 + vardas.size();
-		treciaeil = "* Sveika, " + vardas + "! *";
-	}
-	for (int i = 0; i < ilgis; i++) {
-		pirmaeil = pirmaeil + '*';
-	}
-	penktaeil = pirmaeil;
-	antraeil = '*';
-	for (int i = 1; i < ilgis - 1; i++) {
-		antraeil = antraeil + ' ';
-	}
-	antraeil = antraeil + '*';
-	ketvirtaeil = antraeil;
-	
-	std::cout << pirmaeil << std::endl;
-	std::cout << antraeil << std::endl;
-	std::cout << treciaeil << std::endl;
-	std::cout << ketvirtaeil << std::endl;
-	std::cout << penktaeil << std::endl;
-	
-	return 0;
+void PirmaIrPaskutine(int ilgis);
+void Tuscios(int ilgis);
+void Vidurine(int dydis, std::string vardas);
+
+int main(){
+    int ilgis, dydis;
+    std::string vardas;
+    std::cout << "Iveskite varda: ";
+    std::cin>>vardas;
+    std::cout << "Iveskite remelio dydi: ";
+    std::cin >> dydis;
+
+    if(vardas[vardas.size()-1]=='s'){
+        ilgis = 2 * dydis + 2 + 10 + vardas.size();
+    }
+    else{
+        ilgis = 2 * dydis + 2 + 9 + vardas.size();
+    }
+//Spausdinimas:
+    PirmaIrPaskutine(ilgis);
+    for(int i=0; i<dydis; i++){
+        Tuscios(ilgis);
+    }
+    Vidurine(dydis,vardas);
+    for(int i=0; i<dydis; i++){
+        Tuscios(ilgis);
+    }
+    PirmaIrPaskutine(ilgis);
+
+    return 0;
+}
+
+void PirmaIrPaskutine(int ilgis){
+    for(int i=0; i<ilgis; i++){
+        std::cout<<"*";
+    }
+    std::cout<<"\n";
+}
+
+void Tuscios(int ilgis){
+    std::cout<<"*";
+    for(int i=1; i<ilgis-1; i++){
+        std::cout<<" ";
+    }
+    std::cout<<"*\n";
+}
+
+void Vidurine(int dydis, std::string vardas){
+    std::cout<<"*";
+    for(int i=0; i<dydis; i++){
+        std::cout<<" ";
+    }
+    if(vardas[vardas.size()-1]=='s'){
+        std::cout<<"Sveikas, "<<vardas<<"!";
+    }
+    else{
+        std::cout<<"Sveika, "<<vardas<<"!";
+    }
+    for(int i=0; i<dydis; i++){
+        std::cout<<" ";
+    }
+    std::cout<<"*\n";
+
 }
